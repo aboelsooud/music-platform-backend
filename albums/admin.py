c6ac0ecd6b19bd71c25389862d13dd6dc806f8bd
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-14 19:31:22 +0200

    feat: add all models to admin site

--- albums/admin.py
+++ albums/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Album
 
 # Register your models here.
+
+class AlbumAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Album data', {'fields' : ['name', 'artist',]}),
+        ('Date information', {'fields': ['creation_date', 'release_date']}),
+        (None,  {'fields': ['is_explict']}),
+        (None,  {'fields': ['cost']}),
+    ]
+
+    readonly_fields = ['creation_date']
+
+
+admin.site.register(Album, AlbumAdmin)
--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Artist
 
 # Register your models here.
+
+class ArtistAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Artist data', {'fields': ['stage_name', 'social_link']}),
+    ]
+
+
+admin.site.register(Artist, ArtistAdmin)
