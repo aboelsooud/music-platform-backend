30f3c3d57cf7d290ec72a1df7e462a4149e2cdd1
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-14 19:45:08 +0200

    feat: add another way to show help_text in admin site for is_explicit field

--- albums/admin.py
+++ albums/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import Album
-
+from django import forms
 # Register your models here.
 
+class AlbumForm(forms.ModelForm):
+
+    class Meta:
+        model = Album
+        help_texts = {'is_explict' : 'Approve the album if its name is not explicit'}
+        exclude = () 
+
+
 class AlbumAdmin(admin.ModelAdmin):
+    form = AlbumForm
     fieldsets = [
         ('Album data', {'fields' : ['name', 'artist',]}),
         ('Date information', {'fields': ['creation_date', 'release_date']}),
