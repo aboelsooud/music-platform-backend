5e1cd960e8888f9033b639247031b0716e298f81
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-22 19:45:47 +0200

    feat: create the login page to get user authenticated

--- musicplatform/urls.py
+++ musicplatform/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('artists/', include('artists.urls')),
     path('albums/', include('albums.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
