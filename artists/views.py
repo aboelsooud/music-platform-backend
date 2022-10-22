9e6e0514979b949a4190bb1e7a656e29a1d7541c
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-22 18:39:44 +0200

    feat: change all views to be class based views

--- artists/views.py
+++ artists/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView
+from django.views import View
 from artists.models import Artist
 from .forms import ArtistForm
 from django.contrib import messages
 
 # Create your views here.
 
-def index(request):
-    artists = Artist.objects.all()
-    return render(request, 'artists/artists.html', {'artists' : artists})
+class IndexView(TemplateView):
+    template_name = 'artists/artists.html'
 
-def create_artist(request):
-    form = ArtistForm()
-    if request.method == "POST":
-        form = ArtistForm(request.POST)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['artists'] = Artist.objects.all()
+        return context
+
+class CreateArtistView(View):
+    form_class = ArtistForm
+    initial = {'key': 'value'}
+    template_name = 'artists/artist_form.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         stage_name = request.POST.get('stage_name')
         if form.is_valid:
             try:
@@ -22,6 +34,5 @@ def create_artist(request):
             except:
                 form.save()
                 return redirect('artists')
-    context = {'form': form}
-
-    return render(request, 'artists/artist_form.html', context)
+        context = {'form': form}
+        return render(request, 'artists/artist_form.html', context)
