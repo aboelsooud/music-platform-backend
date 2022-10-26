fcc1d363a55d10a5e1c7d9eb51d9fd5e856e48ca
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-26 14:35:43 +0200

    docs: edit spacings and remove some of unused imports

--- artists/views.py
+++ artists/views.py
@@ -1,7 +1,4 @@
 from artists.models import Artist
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 from .serializers import ArtistSerializer
 from rest_framework import generics
 
