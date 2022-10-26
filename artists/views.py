6b543066a4e2805a2ffd4bd7f64a4d0ca64639e2
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-26 14:33:42 +0200

    feat: switch to generic views in artist model views

--- artists/views.py
+++ artists/views.py
@@ -3,22 +3,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import ArtistSerializer
+from rest_framework import generics
 
 # Create your views here.
 
-class ArtistApiView(APIView):
-    def get(self, request):
-        data = Artist.objects.all()
-        seri = ArtistSerializer(data, many = True)
-        return Response(seri.data)
-    
-    def post(self, request):
-        seri = ArtistSerializer(data = request.data)
-        if seri.is_valid():
-            try:
-                Artist.objects.get(stage_name = seri.validated_data.stage_name)
-            except:
-                seri.save()
-                return Response(seri.data, status=status.HTTP_201_CREATED)
-        
-        return Response(seri.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArtistApiView(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
