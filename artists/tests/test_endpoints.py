8d253c6809bb63f3a275efc325038797b16676ba
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-30 15:00:18 +0200

    feat: create artists endpoints tests

---
+++ artists/tests/test_endpoints.py
@@ -0,0 +1,71 @@
+import pytest
+
+from rest_framework.test import APIClient
+from artists.models import Artist
+from rest_framework import status
+
+@pytest.mark.django_db
+def test_create_an_artist():
+    client = APIClient()
+
+    artist = {
+        'stage_name' : 'Kendrick Lamar'
+    }
+
+    response = client.post('/artists/', artist)
+    data = response.data
+    assert response.status_code == status.HTTP_201_CREATED
+    assert data['id'] == 1
+    assert data['stage_name'] == artist['stage_name']
+    assert 'social_link' in data
+
+@pytest.mark.django_db
+def test_create_artist_with_existing_stage_name():
+    client = APIClient()
+
+    artist = {
+        'stage_name' : 'Kendrick Lamar'
+    }
+
+    client.post('/artists/', artist)
+    
+    artist = {
+        'stage_name' : 'Kendrick Lamar',
+        'social_link' : 'https://www.instagram.com/kendricklamar/'
+    }
+
+    response = client.post('/artists/', artist)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+@pytest.mark.django_db
+def test_retriving_list_of_artists():
+    client = APIClient()
+
+    artist = {
+        'stage_name' : 'Kendrick Lamar'
+    }
+
+    client.post('/artists/', artist)
+
+    artist = {
+        'stage_name' : 'Baby Keem'
+    }
+
+    client.post('/artists/', artist)
+
+    artist = {
+        'stage_name' : 'Frank Ocean'
+    }
+
+    client.post('/artists/', artist)
+
+    response = client.get('/artists/')
+    data = response.data
+
+    assert response.status_code == status.HTTP_200_OK
+
+    artists = Artist.objects.all().values()
+
+    for i in range(len(data)):
+        assert data[i] == artists[i]
