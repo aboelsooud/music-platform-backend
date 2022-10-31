fc42b4429c5fae4f8570428c294983dc02e7d218
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-10-31 18:01:26 +0200

    feat: add post method to albumapi view

--- albums/views.py
+++ albums/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework import pagination
+from dateutil.parser import parse
 
 # Create your views here.
 
@@ -19,3 +20,19 @@ class AlbumApi(GenericAPIView):
         if 'limit' not in request.query_params:
             return Response(serializer.data)
         return self.get_paginated_response(self.paginate_queryset(serializer.data))
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_403_FORBIDDEN, data = {'status': status.HTTP_403_FORBIDDEN,'message':'this user is not authenticated'})
+        
+        if not hasattr(request.user, 'artist'):
+            return Response(status=status.HTTP_403_FORBIDDEN, data = {'status': status.HTTP_403_FORBIDDEN,'message':'this user is not an artist'})
+
+        seri = AlbumSerializer(data = request.data)
+        seri.is_valid(raise_exception=True)
+
+        album = Album.objects.create(name = seri.validated_data['name'], release_date = seri.validated_data['release_date'], cost = seri.validated_data['cost'], artist = request.user.artist)
+        
+        response = AlbumSerializer(album)
+
+        return Response(response.data)
